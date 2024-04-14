5709ee322711d80b23b0edbd606c92be4b6705a0
hadelexyz99 <usmanleye@gmail.com>
2024-04-14 23:43:05 +0100

    new init

--- infra_setup/init.sql
+++ infra_setup/init.sql
@@ -19,7 +19,18 @@ FROM '/data/products.csv' DELIMITER ',' CSV HEADER;
 
 -- TODO: Provide the DDL statment to create this table ALT_SCHOOL.CUSTOMERS
 
+create table if not exists ALT_SCHOOL.CUSTOMERS
+(
+    customer_id  uuid not null primary key,
+    device_id uuid not null,
+    location varchar not null,
+    currency varchar(3)
+);
+
 -- TODO: provide the command to copy the customers data in the /data folder into ALT_SCHOOL.CUSTOMERS
+
+COPY ALT_SCHOOL.PRODUCTS (customer_id, device_id, location, currency)
+FROM '/data/customers.csv' DELIMITER ',' CSV HEADER;
 
 
 
@@ -27,30 +38,48 @@ FROM '/data/products.csv' DELIMITER ',' CSV HEADER;
 create table if not exists ALT_SCHOOL.ORDERS
 (
     order_id uuid not null primary key,
-    -- provide the other fields
+    customer_id uuid not null,
+    status varchar,
+    checked_out_at timestamp
 );
 
 
 -- provide the command to copy orders data into POSTGRES
 
+COPY ALT_SCHOOL.ORDERS (order_id, customer_id, status, checked_out_at)
+FROM '/data/orders.csv' DELIMITER ',' CSV HEADER;
+
+
 
 create table if not exists ALT_SCHOOL.LINE_ITEMS
 (
     line_item_id serial primary key,
-    -- provide the remaining fields
+    order_id uuid not null,
+    item_id bigint not null,
+    quantity bigint
 );
 
 
 -- provide the command to copy ALT_SCHOOL.LINE_ITEMS data into POSTGRES
+
+COPY ALT_SCHOOL.LINE_ITEMS (line_item_id, order_id, item_id, quantity)
+FROM '/data/line_items.csv' DELIMITER ',' CSV HEADER;
+
 
 
 -- setup the events table following the examle provided
 create table if not exists ALT_SCHOOL.EVENTS
 (
     -- TODO: PROVIDE THE FIELDS
+    event_id bigint primary key,
+    
 );
 
 -- TODO: provide the command to copy ALT_SCHOOL.EVENTS data into POSTGRES
+
+COPY ALT_SCHOOL.ORDERS (order_id, customer_id, status, checked_out_at)
+FROM '/data/orders.csv' DELIMITER ',' CSV HEADER;
+
 
 
 
